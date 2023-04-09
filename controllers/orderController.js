808442ffc0b757ece3a92a4aade2070f3a26b12f
ismat27 <adegbiteismail27@gmail.com>
2023-04-09 23:23:57 +0100

    update controller functions for creating new order and fetching orders

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -2,24 +2,73 @@ const {BadRequestError} = require("../errors")
 const {StatusCodes} = require('http-status-codes')
 const Order = require('../models/Order')
 const OrderItem = require('../models/OrderItem')
+const Product = require("../models/Product")
 
 const newOrder = async (req, res) => {
-    const user = req.user
-    const order = await Order.create(req.body)
-    // const order = new Order()
-    // const items = req.body.items
-    // if (!user) {
-    //     throw new BadRequestError('customer not specified')
-    // }
-    res.status(StatusCodes.CREATED).json(req.body)
+    req.body.user = req.user.userId;
+    const customer = req.user.userId;
+    const {
+        shippingAddress,
+        billingAddress,
+        items
+    } = req.body
+    if (!items) {
+        throw new BadRequestError('ordered products cannot be empty')
+    }
+    // loop through the items
+    // verify they are products in the database
+    // calculate the total cost of items using data in the database
+    // consider the discount in the above calculation
+    let total_amount = 0;
+    let total_items = 0;
+    for (const item of items) {
+        const product = await Product.findById(item.id)
+        total_amount += Number(product.price) * Number(item.quantity)
+        total_items += Number(item.quantity)
+    }
+    const orderNumber = String(new Date().getTime())
+    const order = await Order.create({
+        shippingAddress,
+        billingAddress,
+        customer,
+        subtotal: total_amount,
+        total: total_amount,
+        orderNumber
+    })
+    let orderItems = []
+    for (const item of items) {
+        const orderItem = await OrderItem.create({
+            customer: customer,
+            product: item.id,
+            quantity: item.quantity,
+            order: order._id
+        })
+        orderItems.push(orderItem)
+    }
+    order.items = [...orderItems]
+    await order.save()
+    res.status(StatusCodes.CREATED).json({orderNumber, total_amount, total_items})
 }
 
 const allOrders = async (req, res) => {
-    const orders =  await Order.find()
+    const orders =  await Order.find().populate({
+        path: 'items',
+        populate: 'product'
+    })
     res.status(StatusCodes.CREATED).json({orders})
+}
+
+const singleOrder = async (req, res) => {
+    const { orderId } = req.params
+    const order = await Order.findById(orderId).populate({
+        path: 'items',
+        populate: 'product'
+    })
+    res.status(StatusCodes.OK).json({order})
 }
 
 module.exports = {
     newOrder,
     allOrders,
+    singleOrder
 }
