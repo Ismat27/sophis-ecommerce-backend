1e13099330f958e8b7c5b62c0417030079c68d8f
ismat27 <adegbiteismail27@gmail.com>
2023-04-12 14:41:49 +0100

    add route for fetching vendors and for verifying vendors

--- routes/vendorRouter.js
+++ routes/vendorRouter.js
@@ -2,12 +2,15 @@ const express = require('express')
 const { authenticateUser } = require('../middleware/authenticate')
 const {
     createVendorProfile,
-    vendorOrderItems
+    vendorOrderItems,
+    verifyVendor,
+    fetchVendors
 } = require('../controllers/vendorController')
 
 const router = express.Router()
 
-router.route('/').post(createVendorProfile)
+router.route('/').post(createVendorProfile).get(fetchVendors)
 router.route('/:userId/order-items').get(authenticateUser, vendorOrderItems)
+router.route('/verify').patch(verifyVendor)
 
 module.exports = router
